7076345e201a6745f44f6cddd19c782d89a66f9c
hicham-zakroum <hicham.zakroum1@gmail.com>
2023-11-21 12:40:44 +0100

    reload the project and add the hello world test

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hicham zakroum\OneDrive\Desktop\myprojectFlask\.venv
+set VIRTUAL_ENV=C:\Users\hicham zakroum\OneDrive\Desktop\s1_Project\xml\myFlaskProject\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
