5d284dfcb5254f4639fb7533f53ccec4b6487b5c
hicham-zakroum <hicham.zakroum1@gmail.com>
2023-11-24 19:48:13 +0100

    the basic functionality, jsontoxml and xmltojson

--- app/app.py
+++ app/app.py
@@ -1,26 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import xmltodict
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
+@app.route('/jsontoxml', methods=["GET", "POST"])
 def home():
-    # return render_template('home.html', datetoday2=datetoday2)
-    return "<h1>Hi from Flask project</h1>"
+    if request.method == "POST":
+        try:
+            json_str = request.form.get("json")
+            python_dict = json.loads(json_str)
+            xml_string = xmltodict.unparse(python_dict)
+            return render_template("index.html", json=json_str, xml=xml_string, jsonxml=True)
+        except:
+            return "you have to enter a valid data ,Back to hame dir <a href='/'>HOME</a>"
 
 
-# @app.route('/count', methods=['GET', 'POST'])
-# def count():
-#     text = request.form['text']
+    return render_template("index.html",jsonxml=True)
 
-#     words = len(text.split())
 
-#     paras = replace_multiple_newlines(text)
-#     text = text.replace('\r', '')
-#     text = text.replace('\n', '')
-#     chars = len(text)
-#     return render_template('home.html', words=words, paras=paras, chars=chars, datetoday2=datetoday2)
+@app.route('/xmltojson', methods=["GET", "POST"])
+def xmltojson():
+    if request.method == "POST":
+        try:
+            xml_str = request.form.get("xml")
+            json_string = json.dumps(xmltodict.parse(xml_str))
+            return render_template("xmltojson.html", json=json_string, xml=xml_str, jsonxml=False)
+        except:
+            return "you have to enter a valid data ,Back to hame dir <a href='/'>HOME</a>"
 
+    return render_template("xmltojson.html",jsonxml=False)
+
+
+# @app.route("/api/toxml", methods=["POST"])
+# def toxml():
+#     if request.method == "POST":
+#         json_str = request.form.get("json")
+#         python_dict = json.loads(json_str)
+#         xml_string = xmltodict.unparse(python_dict)
+#         return render_template("index.html", response=xml_string)
+#     return ""
 
 # Our main function which runs the Flask App
 if __name__ == '__main__':
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["app/templates/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
